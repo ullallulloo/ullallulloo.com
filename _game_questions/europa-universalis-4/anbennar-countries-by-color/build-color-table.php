8dc7915dfa4dc42b796a136b8128a823c70a7aab
Ullallulloo <ullallulloo@gmail.com>
2024-12-13 17:00:14 -0600

    Add Anbennar country colors

---
+++ _game_questions/europa-universalis-4/anbennar-countries-by-color/build-color-table.php
@@ -0,0 +1,43 @@
+<?php
+
+$data = [];
+
+$country_files = glob(__DIR__."/../../../../\anbennar-eu4-fork-public-build\common\countries/*.txt");
+foreach ($country_files as $country_file) {
+	$name = basename($country_file, ".txt");
+	if (preg_match("/_|\d/", $name))
+		continue;
+	$country_data = file_get_contents($country_file);
+	$color = preg_match("/color\s*=\s*{\s*(\d+\s+\d+\s+\d+)\s*}/", $country_data, $matches);
+	$color = explode(" ", $matches[1]);
+	$color = array_map(fn($x) => intval($x), $color);
+	$data[] = [
+		'name' => $name,
+		'color' => '#'.sprintf('%02x%02x%02x', $color[0], $color[1], $color[2]),
+		'hue' => get_hue(['r' => $color[0], 'g' => $color[1], 'b' => $color[2]]),
+	];
+}
+
+usort($data, fn($a, $b) => $a['hue'] <=> $b['hue']);
+
+echo '<table>';
+echo '<tr><th>Color</th><th>Name</th>';
+foreach ($data as $row) {
+	echo '<tr>';
+	echo '<td style="background-color:'.$row['color'].'" data-color="'.$row['color'].'">&nbsp;</td>';
+	echo '<td>'.$row['name'].'</td>';
+	echo '</tr>';
+}
+echo '</table>';
+
+function get_hue($rgb): float|null {
+	$min = min($rgb);
+	$chroma = max($rgb) - $min;
+	if ($chroma === 0)
+		return null;
+	if ($rgb['r'] === $min)
+		return 60 * (3 - ($rgb['g'] - $rgb['b']) / $chroma);
+	if ($rgb['g'] === $min)
+		return 60 * (5 - ($rgb['b'] - $rgb['r']) / $chroma);
+	return 60 * (1 - ($rgb['r'] - $rgb['g']) / $chroma);
+}
---
+++ _game_questions/europa-universalis-4/anbennar-countries-by-color/index.md
@@ -0,0 +1,14 @@
+---
+title: What colors are Anbennar countries?
+game: Europa Universalis 4
+scripts:
+  - copy_color.js
+---
+
+Whether in normal EU4 or in the land of Halann, the goal is typically to paint the world in your country's color. Thus, if you are going to spend forever staring at a map, you might as well make it a pretty one. This table lists the countries in the Anbennar mod by their colors so you can make sure you are always aesthetic.
+
+{::nomarkdown}
+
+{%- include_relative table.html -%}
+
+{:/nomarkdown}
